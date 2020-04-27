67d8738546701ca0a02fd8699516e3511204d4c2
tettu-rabann <53954788+tettu-rabann@users.noreply.github.com>
2020-04-27 11:57:05 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,11 @@
 <?php
+//MySQLに接続する処理
 try {
   $db = new PDO('mysql:dbname=mini_bbs;
-  host = 192.168.0.14;
-  charset = utf-8', 'root' ,'tettu0302');
+  host = 192.168.0.14;//IPアドレスの番号
+  charset = utf-8', 'root' ,'０００００');　//パスワードは伏せています
 
-} catch (PDOException $e) {
+} catch (PDOException $e) {//エラー処理
   echo 'DB接続エラー： ' . $e->getMessage();
 }
 ?>
